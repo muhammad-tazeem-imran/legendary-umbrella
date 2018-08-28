80ea911cd2d5b7aa2fcb93fa3c292d30fa2f0aba
tazeem <tazeem.imran@arbisoft.com>
2018-08-28 10:53:23 +0500

    W-1.0: minor changes

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ with open('weatherfiles/Murree_weather_2004_Dec.txt',newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     count = 0
     for row in reader:
-        print(row,'\n')
+        print(row["PKT"],'\n')
         count += 1
     print("rows read : ",count)
