9b76c45aa7fcf453275792d015cea3986a3d844d
tazeem <tazeem.imran@arbisoft.com>
2018-08-28 10:19:38 +0500

    W-1.0: started reading in DictReader

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+
+with open('weatherfiles/Murree_weather_2004_Dec.txt',newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    count = 0
+    for row in reader:
+        print(row,'\n')
+        count += 1
+    print("rows read : ",count)
